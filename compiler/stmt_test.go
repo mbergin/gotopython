d35d9086aa341c98011fb6a3fbaeea29a0b089aa
Matt Bergin <mattbergin0@gmail.com>
2017-01-09 14:49:39 +0000

    More statement tests

--- compiler/stmt_test.go
+++ compiler/stmt_test.go
@@ -9,9 +9,12 @@ import (
 	"testing"
 )
 
+const f = py.Identifier("f")
+
 // Placeholders for statement blocks
 var s = []py.Stmt{&py.ExprStmt{Value: &py.Name{Id: py.Identifier("s")}}}
 var t = []py.Stmt{&py.ExprStmt{Value: &py.Name{Id: py.Identifier("t")}}}
+var u = []py.Stmt{&py.ExprStmt{Value: &py.Name{Id: py.Identifier("u")}}}
 
 var one = &py.Num{N: "1"}
 
@@ -19,12 +22,61 @@ var stmtTests = []struct {
 	golang string
 	python []py.Stmt
 }{
+	// Expression statement
 	{"x", []py.Stmt{&py.ExprStmt{Value: x}}},
 
+	// IncDec statements
 	{"x++", []py.Stmt{&py.AugAssign{Target: x, Op: py.Add, Value: one}}},
 	{"x--", []py.Stmt{&py.AugAssign{Target: x, Op: py.Sub, Value: one}}},
 
+	// Assignments
+	{"x = y", []py.Stmt{&py.Assign{Targets: []py.Expr{x}, Value: y}}},
+	{"x = y, z", []py.Stmt{&py.Assign{
+		Targets: []py.Expr{x},
+		Value:   &py.Tuple{Elts: []py.Expr{y, z}},
+	}}},
+	{"x, y = z", []py.Stmt{&py.Assign{
+		Targets: []py.Expr{x, y},
+		Value:   z,
+	}}},
+	{"x, y = y, x", []py.Stmt{&py.Assign{
+		Targets: []py.Expr{x, y},
+		Value:   &py.Tuple{Elts: []py.Expr{y, x}},
+	}}},
+
+	// Short variable declarations
+	{"x := y", []py.Stmt{&py.Assign{Targets: []py.Expr{x}, Value: y}}},
+	{"x := y, z", []py.Stmt{&py.Assign{
+		Targets: []py.Expr{x},
+		Value:   &py.Tuple{Elts: []py.Expr{y, z}},
+	}}},
+	{"x, y := z", []py.Stmt{&py.Assign{
+		Targets: []py.Expr{x, y},
+		Value:   z,
+	}}},
+	{"x, y := y, x", []py.Stmt{&py.Assign{
+		Targets: []py.Expr{x, y},
+		Value:   &py.Tuple{Elts: []py.Expr{y, x}},
+	}}},
+
+	// Branch statements
+	{"break", []py.Stmt{&py.Break{}}},
+	{"continue", []py.Stmt{&py.Continue{}}},
+
+	// If statements
 	{"if x {s}", []py.Stmt{&py.If{Test: x, Body: s}}},
+	{"if s; x {t}", append(s, &py.If{Test: x, Body: t})},
+	{"if x {s} else {t}", []py.Stmt{&py.If{Test: x, Body: s, Orelse: t}}},
+	{"if x {s} else if y {t}", []py.Stmt{&py.If{
+		Test:   x,
+		Body:   s,
+		Orelse: []py.Stmt{&py.If{Test: y, Body: t}},
+	}}},
+	{"if x {s} else if t; y {u}", []py.Stmt{&py.If{
+		Test:   x,
+		Body:   s,
+		Orelse: append(t, &py.If{Test: y, Body: u}),
+	}}},
 }
 
 func parseStmt(stmt string) (ast.Stmt, error) {
